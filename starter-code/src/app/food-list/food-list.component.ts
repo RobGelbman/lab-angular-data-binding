f1b50e89a6f1704aaed58c21a14c9d1710eda942
Robert Gelbman <rgelbman@aol.com>
2018-05-15 21:11:28 -0400

    MIA FTWD - Rob Gelbman - lab-angular-data-binding

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,32 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods:any[];
+  newFood: any ={};
+  toggleForm:boolean = false;
+  todaysFoodList: Array<any> = [];
+  totalCalories: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foodList;
   }
 
+  showForm():void{
+    this.toggleForm = !this.toggleForm;
+  }
+
+  todaysFood(i:number): void{
+    this.todaysFoodList.push(this.foods[i]);
+    this.totalCalories += this.foods[i].calories;
+  }
+
+
+  addFoodItem():void{
+    console.log(this.newFood.name)
+    const addFood = {name: this.newFood.name, calories: this.newFood.calories, image: this.newFood.image};
+    this.foods.push(addFood);
+    this.toggleForm = !this.toggleForm;
+  }
 }
